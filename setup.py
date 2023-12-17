071e312d0f6fd32b17fadd0a6f0a9b5c3fe57df7
ThePilot4571 <boraayhan3737@gmail.com>
2023-12-17 15:31:12 -0700

    a

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="consoldiatepy",
-    version="0.0.1",
+    version="0.0.2",
     description="A Python library for generating a list of strings with every possible combination of placements of a given string (or a zero-width non joiner by default) within another given string, up to a given maximum length of characters.",
     url="https://github.com/ThePilot4571/consolidate-py/", 
     author="ThePilot4571",
@@ -31,7 +31,7 @@ setup(
         "Operating System :: MacOS :: MacOS X"
     ],
     keywords=['python', 'spaces','string','username','spam'],
-    packages=find_packages(where="src"),
+    packages=find_packages(include=['consolidate-py', 'consolidate-py.consolidatepy']),
     install_requires=[],
     project_urls={
         "Source": "https://github.com/ThePilot4571/consolidate-py/",
